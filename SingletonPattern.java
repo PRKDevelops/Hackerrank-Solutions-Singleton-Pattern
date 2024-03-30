52e28e3a0baf9281dc5f7b090c250c8585c64dc0
PRKDevelops <124041839+PRKDevelops@users.noreply.github.com>
2024-03-30 19:21:16 +0530

    Add files via upload

---
+++ SingletonPattern.java
@@ -0,0 +1,13 @@
+
+	import java.io.*;
+	import java.util.*;
+
+	public class SingletonPattern {
+
+	    public static void main(String[] args) {
+	        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+	                    Scanner sc = new Scanner(System.in);
+	        String str = sc.nextLine();
+	        System.out.println("Hello I am a singleton! Let me say " + str + " to you");
+	    }
+	}
